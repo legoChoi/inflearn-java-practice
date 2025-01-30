a3e46881f82f409fc073d2a39aa762c87864dad0
legoChoi <chlx159@gmail.com>
2025-01-30 15:52:45 +0900

    Data Structure Collections - Stack, Queue

---
+++ src/chap15/sec06/exam01/Coin.java
@@ -0,0 +1,14 @@
+package chap15.sec06.exam01;
+
+public class Coin {
+
+    public int value;
+
+    public Coin(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
