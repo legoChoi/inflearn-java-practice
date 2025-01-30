c3e9d35d27d49393501029cd421c1786909a038c
legoChoi <chlx159@gmail.com>
2025-01-30 15:45:19 +0900

    Data Structure Collections - TreeSet, TreeMap and Comparator

---
+++ src/chap15/sec05/exam01/TreeSetExample.java
@@ -0,0 +1,49 @@
+package chap15.sec05.exam01;
+
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+public class TreeSetExample {
+
+    public static void main(String[] args) {
+        TreeSet<Integer> scores = new TreeSet<>();
+
+        scores.add(87);
+        scores.add(98);
+        scores.add(75);
+        scores.add(95);
+        scores.add(80);
+
+        for (Integer s : scores) {
+            System.out.println(s + " ");
+        }
+        System.out.println();
+
+        System.out.println("가장 낮은 점수 (scores.first()) = " + scores.first());
+        System.out.println("가장 높은 점수 (scores.last()) = " + scores.last());
+        System.out.println("95점 아래 점수 (scores.lower(95)) = " + scores.lower(95));
+        System.out.println("95점 위의 점수 (scores.higher(95)) = " + scores.higher(95));
+        System.out.println("95점이거나 바로 아래 점수 (scores.floor(95)) = " + scores.floor(95));
+        System.out.println("85점이거나 바로 위의 점수 (scores.ceiling(85)) = " + scores.ceiling(85) + "\n");
+
+        // 내림차순 정렬
+        NavigableSet<Integer> descendingScores = scores.descendingSet();
+        for (Integer s : descendingScores) {
+            System.out.println(s + " ");
+        }
+        System.out.println();
+
+        // 범위 검색 80 <= score
+        NavigableSet<Integer> rangeSet = scores.tailSet(80, true);
+        for (Integer s : rangeSet) {
+            System.out.println(s + " ");
+        }
+        System.out.println();
+
+        // 범위 검색 80 <= score < 90
+        rangeSet = scores.subSet(80, true, 90, false);
+        for (Integer s : rangeSet) {
+            System.out.println(s + " ");
+        }
+    }
+}
