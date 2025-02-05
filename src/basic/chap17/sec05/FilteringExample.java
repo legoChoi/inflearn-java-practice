d7e39856b2518246132b4660deb3ff62349034a1
legoChoi <chlx159@gmail.com>
2025-02-05 22:59:31 +0900

    Stream - filter()

---
+++ src/basic/chap17/sec05/FilteringExample.java
@@ -0,0 +1,29 @@
+package basic.chap17.sec05;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilteringExample {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("홍길동");
+        list.add("신용권");
+        list.add("감자바");
+        list.add("신용권");
+        list.add("신민철");
+
+        list.forEach(System.out::println);
+        System.out.println();
+
+        list.stream()
+                .distinct()
+                .forEach(System.out::println);
+        System.out.println();
+
+        list.stream()
+                .distinct()
+                .filter(s -> s.startsWith("신"))
+                .forEach(System.out::println);
+    }
+}
