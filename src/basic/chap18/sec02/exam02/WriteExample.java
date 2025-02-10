8abf51ba93f776ec6225b13b168faec1c972ad68
legoChoi <chlx159@gmail.com>
2025-02-10 19:18:50 +0900

    데이터 입출력 - 바이트 출력 스트림
    - 파일 저장

---
+++ src/basic/chap18/sec02/exam02/WriteExample.java
@@ -0,0 +1,23 @@
+package basic.chap18.sec02.exam02;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class WriteExample {
+
+    public static void main(String[] args) {
+        try {
+            OutputStream os = new FileOutputStream("./src/basic/chap18/sec02/exam02/test2.db");
+
+            byte[] array = {10, 20, 30};
+
+            os.write(array);
+
+            os.flush();
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
